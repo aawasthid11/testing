4c5e60d153c683014512a3a76180a69064266444
Abhishek Awasthi <abhishek.awasthi@dream11.com>
2022-09-20 18:46:17 +0530

    Commiting some unformatted code

---
+++ src/formaterTest.java
@@ -0,0 +1,13 @@
+public class formaterTest {
+    private int variable1;
+    public static void main(String[] args){
+        int a=5;
+        int b=10;
+                                                    System.out.println("Pretty unformated code line 1");
+        System
+                .out
+                .println(
+                        "Pretty unformated code line2"
+                );
+    }
+}
